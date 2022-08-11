4403f88e134083275d0ae3d3b1f20b606c352101
Boldfive <abisoyeomolade11@gmail.com>
2022-08-11 14:19:57 -0700

    10-top_score.sql

---
+++ 0x0D-SQL_introduction/10-top_score.sql
@@ -0,0 +1,3 @@
+-- Script that lists all records of the table second_table of the database hbtn_0c_0
+-- List all records of the `second_table` by `score` in descending order
+SELECT score, name FROM `second_table` ORDER BY score desc;
