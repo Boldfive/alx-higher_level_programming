aa46e45a341875d77ce11deeeaeef99c1386c10a
Boldfive <abisoyeomolade@gmail.com>
2022-09-22 14:31:04 -0700

    4-header.sh

--- 0x10-python-network_0/4-header.sh
+++ 0x10-python-network_0/4-header.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-# A Bash script that takes in a URL as an argument, sends a GET request to the URL, and displays the body of the response.
+# A bah script that takes in a URL as an argument, Send a get request with custom set HEADER variable
 curl -s "$1" -X GET -H "X-School-User-Id: 98"
