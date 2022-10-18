7fc8ad938713d8ae1b4def2e1b63891f75c04c6c
Boldfive <abisoyeomolade11@gmail.com>
2022-10-18 19:30:42 +0000

    3-starwars_title.js

--- 0x14-javascript-web_scraping/3-starwars_title.js
+++ 0x14-javascript-web_scraping/3-starwars_title.js
@@ -1,8 +1,11 @@
 #!/usr/bin/node
 const request = require('request');
-const epId = process.argv[2];
-const apiUrl = `http://swapi.co/api/films/${epId}`;
-request.get(apiUrl, (error, response, body) => {
-  if (error) console.log(error);
-  console.log(JSON.parse(body).title);
+const end = process.argv[2];
+
+request('https://swapi-api.hbtn.io/api/films/' + end, function (error, respones, body) {
+  if (error) {
+    console.log(error);
+  } else {
+    console.log(JSON.parse(body).title);
+  }
 });
